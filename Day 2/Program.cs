73fe54356f0db579c211db1ee75c8baa29424714
John Conacher <conacherjohn@gmail.com>
2022-01-05 21:28:43 +0000

    Add files via upload

---
+++ Day 2/Program.cs
@@ -0,0 +1,27 @@
+﻿string[] lines = System.IO.File.ReadAllLines(@"C:\Users\conac\Documents\Advent Of Code\Day 2\input.txt");
+
+int horizpos = 0;
+int depth = 0;
+int aim = 0;
+
+foreach (string line in lines) {
+    string[] l = line.Split();
+    string dir = l[0];
+    int X = Convert.ToInt32(l[1]);
+        
+    if (dir.Equals("forward")) {
+        horizpos += X;
+        depth += aim * X; // remove for morning
+    } else {
+        if (dir.Equals("down")) {
+            aim += X;
+        } else if (dir.Equals("up")) {
+            aim -= X;
+        }
+        // depth = aim;
+    }
+}
+
+Console.WriteLine(horizpos);
+Console.WriteLine(depth);
+Console.WriteLine(depth*horizpos);
